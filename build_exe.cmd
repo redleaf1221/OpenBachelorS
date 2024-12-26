aedc6f5540cc1e9e900f3a701a507ea865096aa8
pfyy <112098515+pfyy@users.noreply.github.com>
2024-12-27 05:44:33 +0800

    exe add aria2

--- build_exe.cmd
+++ build_exe.cmd
@@ -1,3 +1,3 @@
 pip install pyinstaller==6.11.1
-pyinstaller -p . --contents-directory . --add-data conf:conf --add-data data:data --add-data res:res main.py
+pyinstaller -p . --contents-directory . --add-data conf:conf --add-data data:data --add-data res:res --add-data aria2c.exe:. main.py
 pause
