60b3cac6349209558093ab904d9169898cda63f8
pfyy <112098515+pfyy@users.noreply.github.com>
2025-01-21 07:10:38 +0800

    sandbox squad

--- src/bp/bp_sandboxPerm.py
+++ src/bp/bp_sandboxPerm.py
@@ -9,9 +9,42 @@ from ..util.player_data import player_data_decorator
 bp_sandboxPerm = Blueprint("bp_sandboxPerm", __name__)
 
 
+class SandboxBasicManager:
+    def __init__(self, player_data, topic_id, request_json, response):
+        self.player_data = player_data
+        self.topic_id = topic_id
+        self.request_json = request_json
+        self.response = response
+
+    def sandboxPerm_sandboxV2_setSquad(self):
+        squad_lst = self.player_data["sandboxPerm"]["template"]["SANDBOX_V2"][
+            self.topic_id
+        ]["troop"]["squad"].copy()
+
+        squad_idx = self.request_json["index"]
+
+        squad_lst[squad_idx]["slots"] = self.request_json["slots"]
+        squad_lst[squad_idx]["tools"] = self.request_json["tools"]
+
+        self.player_data["sandboxPerm"]["template"]["SANDBOX_V2"][self.topic_id][
+            "troop"
+        ]["squad"] = squad_lst
+
+
+def get_sandbox_manager(player_data, topic_id, request_json, response):
+    return SandboxBasicManager(player_data, topic_id, request_json, response)
+
+
 @bp_sandboxPerm.route("/sandboxPerm/sandboxV2/setSquad", methods=["POST"])
 @player_data_decorator
 def sandboxPerm_sandboxV2_setSquad(player_data):
     request_json = request.get_json()
     response = {}
+
+    topic_id = request_json["topicId"]
+
+    sandbox_manager = get_sandbox_manager(player_data, topic_id, request_json, response)
+
+    sandbox_manager.sandboxPerm_sandboxV2_setSquad()
+
     return response
