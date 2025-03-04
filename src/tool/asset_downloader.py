abb3b12d1c535a456a0ee8aecd36a899cf89768c
pfyy <112098515+pfyy@users.noreply.github.com>
2025-03-05 00:58:18 +0800

    parallel download

--- src/tool/asset_downloader.py
+++ src/tool/asset_downloader.py
@@ -1,5 +1,7 @@
 import os
 import json
+from multiprocessing import Pool
+
 
 from ..app import app
 from ..const.json_const import true, false, null
@@ -11,33 +13,46 @@ from ..bp.bp_assetbundle import (
 )
 from ..util.helper import get_asset_filename
 
+NUM_ASSET_DOWNLOAD_WORKER = 8
+
+
+def asset_download_worker_func(worker_param):
+    res_version, asset_filename = worker_param
+
+    print(f"info: downloading {asset_filename}")
+
+    with app.test_request_context():
+        ret_val = assetbundle_official_Android_assets(res_version, asset_filename)
+
+    if isinstance(ret_val, tuple):
+        print(f"err: failed to download {asset_filename}")
+
 
 if __name__ == "__main__":
+    res_version = const_json_loader[VERSION_JSON]["version"]["resVersion"]
+
     with app.test_request_context():
-        res_version = const_json_loader[VERSION_JSON]["version"]["resVersion"]
-
         assetbundle_official_Android_assets(res_version, HOT_UPDATE_LIST_JSON)
-        with open(
-            os.path.join(ASSET_DIRPATH, res_version, HOT_UPDATE_LIST_JSON),
-            encoding="utf-8",
-        ) as f:
-            hot_update_list = json.load(f)
+    with open(
+        os.path.join(ASSET_DIRPATH, res_version, HOT_UPDATE_LIST_JSON),
+        encoding="utf-8",
+    ) as f:
+        hot_update_list = json.load(f)
 
-        asset_filename_lst = []
+    asset_filename_lst = []
 
-        for ab_obj in hot_update_list["abInfos"]:
-            ab_filename = get_asset_filename(ab_obj["name"])
+    for ab_obj in hot_update_list["abInfos"]:
+        ab_filename = get_asset_filename(ab_obj["name"])
 
-            asset_filename_lst.append(ab_filename)
+        asset_filename_lst.append(ab_filename)
 
-        for pack_obj in hot_update_list["packInfos"]:
-            pack_filename = get_asset_filename(pack_obj["name"])
+    for pack_obj in hot_update_list["packInfos"]:
+        pack_filename = get_asset_filename(pack_obj["name"])
 
-            asset_filename_lst.append(pack_filename)
+        asset_filename_lst.append(pack_filename)
 
-        for asset_filename in asset_filename_lst:
-            print(f"info: downloading {asset_filename}")
-            ret_val = assetbundle_official_Android_assets(res_version, asset_filename)
-
-            if isinstance(ret_val, tuple):
-                print(f"err: failed to download {asset_filename}")
+    with Pool(NUM_ASSET_DOWNLOAD_WORKER) as pool:
+        pool.map(
+            asset_download_worker_func,
+            [(res_version, asset_filename) for asset_filename in asset_filename_lst],
+        )
