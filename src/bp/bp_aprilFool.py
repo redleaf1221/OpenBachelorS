a1e10a574e031e38a1f74179904dcbf620c663fc
pfyy <112098515+pfyy@users.noreply.github.com>
2025-04-02 08:52:56 +0800

    april live

--- src/bp/bp_aprilFool.py
+++ src/bp/bp_aprilFool.py
@@ -2,7 +2,7 @@ from flask import Blueprint
 from flask import request
 
 from ..const.json_const import true, false, null
-from ..const.filepath import CONFIG_JSON, VERSION_JSON
+from ..const.filepath import CONFIG_JSON, VERSION_JSON, ACTIVITY_TABLE
 from ..util.const_json_loader import const_json_loader
 from ..util.player_data import player_data_decorator
 from ..util.battle_log_logger import log_battle_log_if_necessary
@@ -68,5 +68,60 @@ def aprilFool_act5fun_battleFinish(player_data):
         },
         "playerResult": {"totalWin": 10, "streak": 10, "totalRound": 10},
         "reward": [],
+    }
+    return response
+
+
+@bp_aprilFool.route("/aprilFool/act4fun/battleStart", methods=["POST"])
+@player_data_decorator
+def aprilFool_act4fun_battleStart(player_data):
+    request_json = request.get_json()
+    response = {
+        "result": 0,
+        "battleId": "00000000-0000-0000-0000-000000000000",
+    }
+    return response
+
+
+@bp_aprilFool.route("/aprilFool/act4fun/battleFinish", methods=["POST"])
+@player_data_decorator
+def aprilFool_act4fun_battleFinish(player_data):
+    request_json = request.get_json()
+
+    log_battle_log_if_necessary(player_data, request_json["data"])
+
+    activity_table = const_json_loader[ACTIVITY_TABLE]
+
+    material_lst = []
+
+    material_inst_id = 0
+
+    for material_id, material_obj in activity_table["actFunData"]["act4FunData"][
+        "spMatDict"
+    ]:
+        material_lst.append(
+            {
+                "instId": material_inst_id,
+                "materialId": material_id,
+                "materialType": 1,
+            }
+        )
+
+        material_inst_id += 1
+
+    response = {
+        "materials": material_lst,
+        "liveId": "00000000-0000-0000-0000-000000000000",
+    }
+    return response
+
+
+@bp_aprilFool.route("/aprilFool/act4fun/liveSettle", methods=["POST"])
+@player_data_decorator
+def aprilFool_act4fun_liveSettle(player_data):
+    request_json = request.get_json()
+
+    response = {
+        "ending": "goodending_1",
     }
     return response
